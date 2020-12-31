7730981c628dc1820d4752734f940308a34539aa
basura-persistent <57583373+basura-persistent@users.noreply.github.com>
2020-12-30 23:28:59 -0800

    Update connector.js

--- apiDemo/database/connector.js
+++ apiDemo/database/connector.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 
 const dbConnection = mysql.createConnection({
-    debug: false,
-    host: '127.0.0.1',
-    port: '3306',
-    user: 'rbasu_cs355fl20',
-    password: 'ba4826073',
-    database: 'rbasu_cs355fl20'
+    debug: 
+    host: 
+    port: 
+    user: 
+    password: 
+    database: 
 });
 
 module.exports = dbConnection;
